73c6c0da7b8e3eb0b95174c9d58bf4e67d0a935b
Neelabhro Roy <neelabhro16171@iiitd.ac.in>
2021-02-23 01:51:40 +0100

    Updated File

--- qpsk.m
+++ qpsk.m
@@ -28,4 +28,33 @@ function d = qpsk(b)
 % Output:
 %   d = complex-valued QPSK symbols
 
+%Just for simulation and cross checking, comment before running the final
+%code
+%nr_data_bits=1000;
+%b_data = (randn(1, nr_data_bits) > .5);
+%b = [b_data];
+
+d = zeros(1, length(b)/2);
+
+for n = 1:length(b)/2
+    odd = b(2*n);
+    even = b(2*n-1);
+    if (even == 0) && (odd == 0)
+        d(n) = exp(1i*pi/4);   % This denotes 45'
+    end
+    if (even == 1) && (odd == 0)
+        d(n) = exp(1i*3*pi/4); % This denotes 135'
+    end
+    if (even == 1) && (odd == 1)
+        d(n) = exp(1i*5*pi/4); % This denotes 225'
+    end
+    if (even == 0) && ( odd== 1)
+        d(n) = exp(1i*7*pi/4); % This denotes 315'
+    end
+end
+
+figure;
+plot(d,'x');
+xlabel('Real'); ylabel('Imaginary');
+title('QPSK constellation');
 
